9bb6cee40fb219e1e8ff417727a3c1166d38422d
Ilnura <t.ilnura@gmail.com>
2021-10-18 19:31:23 -0600

    add index.html, style.css, script.js. Structure, styles added

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+console.log(hamburger);
+const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+	hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+	 navBar.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove(".active");
+    navMenu.classList.remove(".active");
+}
