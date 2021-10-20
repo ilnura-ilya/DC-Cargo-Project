b2a360bed790b90991ec3c47b59e3db647454a93
Ilnura <t.ilnura@gmail.com>
2021-10-20 14:30:56 -0600

    working on responsibility

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//Mobile menu
 const hamburger = document.querySelector(".hamburger");
-console.log(hamburger);
 const navMenu = document.querySelector(".nav-menu");
 const navBar = document.querySelector(".navbar");
+const navLink = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -11,11 +12,25 @@ function mobileMenu() {
 	 navBar.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
-
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburger.classList.remove(".active");
     navMenu.classList.remove(".active");
 }
+
+//Typing of text
+const chText = 'это быстрая и надежная доставка вашего груза из Китая! звони нам по номеру +996701544405';
+
+let i = 0;
+let speed = 70;
+
+function type(){
+    if (i < chText.length) {
+    document.querySelector('#charAtText').textContent += chText.charAt(i);
+    i++;
+    setTimeout(type, speed);
+    }
+}
+type();
+
